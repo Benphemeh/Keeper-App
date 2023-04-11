338609f8f6dab53936770d4192477f06470814f9
Benphemeh <Beniphemeh11@yahoo.com>
2023-04-11 05:57:21 +0100

    completed

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Footer() {
+  const year = new Date().getFullYear();
+  return (
+    <footer>
+      <p>Copyright O'Ben Brands ⓒ {year}</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <h1> O'Ben Keeper App</h1>
+    </header>
+  );
+}
+
+
+export default Header;
